762e4d544e9a79a8bfe003621c98652808a49e31
SKB <973689813@qq.com>
2018-12-01 22:14:33 +0800

    Create 归并排序.cpp

---
+++ 归并排序.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void Merge(int a[],int start,int mid,int end,int t[]){
+    int p=0,p1=start,p2=mid+1;
+    while(p1<=mid&&p2<=end){
+        if(a[p1]<a[p2])t[p++]=a[p1++];
+        else t[p++]=a[p2++];
+    }
+    while(p1<=mid)t[p++]=a[p1++];
+    while(p2<=end)t[p++]=a[p2++];
+    for(int i=0;i<=end-start;i++)a[start+i]=t[i];
+}
+void MergeSort(int a[],int start,int end,int t[]){
+    if(start<end){
+        int m=start+(end-start)/2;
+        MergeSort(a,start,m,t);
+        MergeSort(a,m+1,end,t);
+        Merge(a,start,m,end,t);
+    }
+}
+int main(){
+    int n[]={1,6,4,8,9,4,60,59,4345,847,23,4,55,935,34,89};
+    int t[20];
+    MergeSort(n,0,15,t);
+    for(int i=0;i<=15;i++)cout<<t[i]<<' ';    
+    cout<<endl;
+    return 0;
+}
